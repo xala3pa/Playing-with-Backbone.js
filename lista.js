8ab4bf1c851641ce94ca57c7aebcaf00de3c0674
Alvaro <salazar3pa@gmail.com>
2012-06-03 21:11:37 +0200

    Solucionados problemas del codificado del desarrollo

--- lista.js
+++ lista.js
@@ -1,43 +1,2 @@
 $(function() {
-
-Todo = Backbone.Model.extend({
-//Modelo de soporte para nuestras tareas
-name: null
-});
-
-//Listado de TODOS
-TodoList = Backbone.Collection.extend({
-    initialize: function(){
-        this.bind("add", function( model ){
-            view.render( model );
-        })
-    }
-});
-
-//Vista de los TODOS
-TodoView = Backbone.View.extend({
-
-    tagName: 'li',
-
-    events: {
-        'click #add-input':  'getTodo',
-    },
-
-    initialize: function() {
-        this.todolist = new TodoList();
-        _.bindAll(this, 'render');
-    }, 
-
-    getTodo: function() {
-        var todo = $('#input').val();
-        this.todolist.add( {name: todo} );
-    },
-
-    render: function( model ) {
-        $("#todo-list").append("<li>"+ model.get("name")+"</li>");
-    },
-
-});
-
-var view = new TodoView({el: 'body'});
-});​
+Todo = Backbone.Model.extend({//Modelo de soporte para nuestras tareasname: null});//Listado de TODOSTodoList = Backbone.Collection.extend({    initialize: function(){        this.bind("add", function( model ){            view.render( model );        });    }});//Vista de los TODOSTodoView = Backbone.View.extend({    tagName: 'li',    events: {        'click #add-input':  'getTodo'    },    initialize: function() {        this.todolist = new TodoList();         _.bindAll(this, 'render');    },     getTodo: function() {        var todo = $('#input').val();        this.todolist.add( {name: todo} );    },    render: function( model ) {        $("#todo-list").append("<li>"+ model.get("name")+"</li>");    }});var view = new TodoView({el: 'body'});});
